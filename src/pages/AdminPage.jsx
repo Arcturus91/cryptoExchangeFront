1da8fb7b45927a4e3e5ac08e42e653500ba9c6a1
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-09-16 21:41:52 -0500

    adminPage 100%

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,17 +1,21 @@
-import { getAllAssets,addInventory,addCash,createCrypto } from "../services/admin-ws";
-import { Modal, Typography, Button, Col, Row, Form, Input } from "antd";
+import {
+  getAllAssets,
+  addInventory,
+  addCash,
+  createCrypto,
+} from "../services/admin-ws";
+import { Modal, Typography, Button, Col, Row, Form, Input, Divider } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { InventoryTable } from "../components";
-const { Title } = Typography;
+const { Title,Paragraph,Text } = Typography;
 
 const AdminPage = (props) => {
   const [inventory, setInventory] = useState();
   const [financeObj, setFinanceObj] = useState();
   const [cashForm, setcashForm] = useState(false);
-  const [addInventoryForm, setaddInventoryForm] = useState(false)
-  const [addCryptoForm, setaddCryptoForm] = useState(false)
-
+  const [addInventoryForm, setaddInventoryForm] = useState(false);
+  const [addCryptoForm, setaddCryptoForm] = useState(false);
 
   const addCashHandler = () => {
     setcashForm((prevState) => {
@@ -19,41 +23,38 @@ const AdminPage = (props) => {
     });
   };
 
-  const addInventoryHandler = ()=>{
+  const addInventoryHandler = () => {
     setaddInventoryForm((prevState) => {
       return !prevState;
-      
     });
-  }
+  };
 
-  const addNewCryptoHandler = ()=>{
+  const addNewCryptoHandler = () => {
     setaddCryptoForm((prevState) => {
       return !prevState;
     });
-  }
-
- 
+  };
 
   const onFinish = (values) => {
-    console.log("lo que envio",values)
+    console.log("lo que envio", values);
 
-    if(values && cashForm){
-    addCash(values).then((res) => {
-      const { data, status, errorMessage } = res;
-      console.log("Success:", data);
-      if (status) {
-        Modal.success({
-          content: "Todo exitoso. Se agregó el cash",
-        });
-        setcashForm((prevState) => {
-          return !prevState;
-        });
-        return;
-      } else {
-        Modal.error({ content: errorMessage });
-      }
-    })} else if (values && addInventoryForm){
-
+    if (values && cashForm) {
+      addCash(values).then((res) => {
+        const { data, status, errorMessage } = res;
+        console.log("Success:", data);
+        if (status) {
+          Modal.success({
+            content: "Todo exitoso. Se agregó el cash",
+          });
+          setcashForm((prevState) => {
+            return !prevState;
+          });
+          return;
+        } else {
+          Modal.error({ content: errorMessage });
+        }
+      });
+    } else if (values && addInventoryForm) {
       addInventory(values).then((res) => {
         const { data, status, errorMessage } = res;
         console.log("Success:", data);
@@ -68,9 +69,8 @@ const AdminPage = (props) => {
         } else {
           Modal.error({ content: errorMessage });
         }
-      })
-
-    } else if (values && addCryptoForm){
+      });
+    } else if (values && addCryptoForm) {
       createCrypto(values).then((res) => {
         const { data, status, errorMessage } = res;
         console.log("Success:", data);
@@ -85,17 +85,13 @@ const AdminPage = (props) => {
         } else {
           Modal.error({ content: errorMessage });
         }
-      })
-
+      });
     }
-
   };
 
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
-
-
 
   useEffect(() => {
     getAllAssets().then((res) => {
@@ -103,154 +99,38 @@ const AdminPage = (props) => {
       if (status) {
         setInventory(data.totalInventory);
         data.finances.map((item) => setFinanceObj(item));
-
       } else {
         Modal.error({ content: errorMessage });
       }
     });
-  }, [cashForm,addInventoryForm,addCryptoForm]);
+  }, [cashForm, addInventoryForm, addCryptoForm]);
 
   return (
     <div className="admin-page">
       <Title level={2}>Bienvenido administrador: {props.user.firstName}</Title>
 
-<Row>
-      <Title level={3}>Los activos del negocio:</Title>
+<div style={{paddingLeft:200,paddingRight:200,paddingBottom:20}}>
+      <Title level={3}>Instrucciones</Title>
+    <Paragraph>
+     En esta página encontrarás la lista de cripto monedas que AlpacaExchange tiene en su inventario.
+     Cualquier cripto que veas que está en baja disponibilidad, la repones. Presionas 
+      <Text code>Comprar Inventario</Text> para eso. Si falta efectivo en las cuentas, presiona <Text code>Agregar Cash</Text>).
+      Finalmente, si no tenemos la crypto y quieres registrarla, presiona <Text code>Registrar nueva crypto</Text>.
+    </Paragraph>
 
+</div>
 
+<Row gutter={[8, 16]}>
 
-
-
-      <InventoryTable inventory={inventory} />
-
-{/* --------------------- */}
-
-
- <Button
-            onClick={() => {
-              addNewCryptoHandler();
-            }}
-          >
-            {" "}
-           Registrar nueva crypto{" "}
-          </Button>
-
-
-      {addCryptoForm ? (
-            <Form
-              name="addCrypto"
-              labelCol={{
-                span: 8,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Form.Item 
-                    rules={[
-          {
-            required: true,
-            message: "Porfavor, define la criptomoneda a registrar.",
-          },
-        ]}  
-              
-               label="Criptomoneda a registrar" name="cryptoName">
-                <Input  />
-              </Form.Item>
-              <Form.Item
-              rules={[
-          {
-            required: true,
-            message: "Porfavor, define la cantidad a comprar.",
-          },
-        ]}  
-               label="Cantidad" name="coinQuantity">
-                <Input />
-              </Form.Item>
-
-              <Form.Item
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
-              >
-                <Button type="primary" htmlType="submit">
-                  Registrar
-                </Button>
-              </Form.Item>
-            </Form>
-          ) : null}
-
-
-
-
-
-
-{/* --------------------- */}
-      <Button
-            onClick={() => {
-              addInventoryHandler();
-            }}
-          >
-            {" "}
-           Comprar inventario{" "}
-          </Button>
-
-
-      {addInventoryForm ? (
-            <Form
-              name="addInventory"
-              labelCol={{
-                span: 8,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Form.Item label="Criptomoneda a comprar" name="cryptoName">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Cantidad" name="cryptoBuyAmount">
-                <Input />
-              </Form.Item>
-
-              <Form.Item
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
-              >
-                <Button type="primary" htmlType="submit">
-                  Comprar
-                </Button>
-              </Form.Item>
-            </Form>
-          ) : null}
-          </Row>
-      
-
-      <Row>
-        <Col span={8}></Col>
-        <Col span={8}>
-          <Title level={3}>
+<Col span={6} />
+        <Col span={6}>
+          <Title level={5}>
             Efectivo en cuentas:{" "}
-            {financeObj ? financeObj.cash : <LoadingOutlined />} USD
+            {financeObj ? financeObj.cash.toFixed(2) : <LoadingOutlined />} USD
           </Title>
         </Col>
-        <Col span={8}>
-          <Button
+        <Col span={6}>
+          <Button type="primary"
             onClick={() => {
               addCashHandler();
             }}
@@ -258,19 +138,21 @@ const AdminPage = (props) => {
             {" "}
             Agregar cash{" "}
           </Button>
+</Col>
+<Col span={6} />
 
+<Col span={24}>
           {cashForm ? (
             <Form
               name="addCash"
               labelCol={{
-                span: 8,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
-              initialValues={{
-                remember: true,
-              }}
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 10,
+        }}
+
+
               onFinish={onFinish}
               onFinishFailed={onFinishFailed}
               autoComplete="off"
@@ -280,10 +162,10 @@ const AdminPage = (props) => {
               </Form.Item>
 
               <Form.Item
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
+              wrapperCol={{
+                offset: 8,
+                span: 10,
+              }}
               >
                 <Button type="primary" htmlType="submit">
                   Depositar
@@ -291,15 +173,147 @@ const AdminPage = (props) => {
               </Form.Item>
             </Form>
           ) : null}
-        </Col>
-      </Row>
+          </Col>
+        
+      
 
       <Col span={24}>
-        <Title level={3}>
+        <Title level={5}>
           Ganancias Acumuladas:{" "}
           {financeObj ? financeObj.profits : <LoadingOutlined />} USD
         </Title>
       </Col>
+
+      <Divider/>
+
+
+<Col span={12}>
+        <Button type="primary"
+          onClick={() => {
+            addNewCryptoHandler();
+          }}
+        >
+          {" "}
+          Registrar nueva crypto{" "}
+        </Button>
+
+        {addCryptoForm ? (
+          <Form
+          className="add-accounts"
+            name="addCrypto"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 10,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: "Porfavor, define la criptomoneda a registrar.",
+                },
+              ]}
+              label="Criptomoneda a registrar"
+              name="cryptoName"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: "Porfavor, define la cantidad a comprar.",
+                },
+              ]}
+              label="Cantidad"
+              name="coinQuantity"
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+ wrapperCol={{
+                offset: 8,
+                span: 10,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Registrar
+              </Button>
+            </Form.Item>
+          </Form>
+        ) : null}
+
+        </Col>
+
+
+<Col span={12}>
+        <Button type="primary"
+          onClick={() => {
+            addInventoryHandler();
+          }}
+        >
+          {" "}
+          Comprar inventario{" "}
+        </Button>
+
+        {addInventoryForm ? (
+          <Form
+            name="addInventory"
+            className="add-accounts"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 10,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item label="Criptomoneda a comprar" name="cryptoName">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Cantidad" name="cryptoBuyAmount">
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+   wrapperCol={{
+                offset: 8,
+                span: 10,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Comprar
+              </Button>
+            </Form.Item>
+          </Form>
+        ) : null}
+        </Col>
+
+
+
+
+<Divider/>
+
+<Col span={24}>
+        <InventoryTable inventory={inventory} />
+</Col>
+
+</Row>
+      
     </div>
   );
 };
