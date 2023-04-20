534cd46271775e23c48b4736325cb7d662644fd2
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-04-19 19:38:25 -0500

    Define production server as localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 //validate if app is running on producion or local
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 //2 environment
-                                                                        //"http://localhost:5005/api/auth/login"
-const baseURL = isProduction ? 'https://alpacaexchange.herokuapp.com/api' : "http://localhost:5005/api";
+//"http://localhost:5005/api/auth/login"
+const baseURL =
+  /* isProduction ? 'https://alpacaexchange.herokuapp.com/api' : */ "http://localhost:5005/api";
 
 export const api = axios.create({
-    baseURL,
-    withCredentials:true,//cookies
-timeout:10000,
-})
+  baseURL,
+  withCredentials: true, //cookies
+  timeout: 10000,
+});
 
-export const uploadURL = `${baseURL}/user/my-profile/singleUpload`
+export const uploadURL = `${baseURL}/user/my-profile/singleUpload`;
 
 ///
 //http://localhost:5005/api/user/my-profile/singleUpload
